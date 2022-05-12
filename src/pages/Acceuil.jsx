09a8095eba09cc1cbd52f94ebbf452c94150a4a6
Vinceboxe <vincent.dudouet@gmail.com>
2022-05-12 16:08:35 +0200

    routes et link over

--- src/pages/Acceuil.jsx
+++ src/pages/Acceuil.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Acceuil = () => {
   return (
     <div className="acceuil">
       <h1 className="title"> WORLD WORST WASTER</h1>
-      <h2>Choose your voice</h2>
+      <h2>Choose your path</h2>
       <div className="button-choice">
-        <button className="Trump"></button>
-        <button className="Greta"></button>
+        <Link to="/HomeTrump">
+          <button className="Trump">trump</button>
+        </Link>
+        <Link to="/HomeGreta">
+          <button className="Greta"> greta</button>
+        </Link>
       </div>
     </div>
   );
--- src/pages/HomeGreta.jsx
+++ src/pages/HomeGreta.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+// import HeaderG from "@components/HeaderG";
 
 const HomeGreta = () => {
   return (
-    <div>
-      <h1 className="trump-title">
-        Choose the place where to live, if you want very green
-      </h1>
-      <button>LET'S GO</button>
-    </div>
+    <>
+      <div>
+        <h1 className="greta-title">
+          Choose the place where to live, if you want very green
+        </h1>
+        <button>LET'S GO</button>
+      </div>
+    </>
   );
 };
 
