6966ed5c479abb8325214b6fc5928da9ff81d037
BDF007 <bdf007@gmail.com>
2022-05-12 16:23:21 +0200

    apple API selon gaz

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import "./App.css";
+import Appel from "./componants/Appel";
 
 export default function App() {
-  return <h1>Hello Vite React App</h1>;
+  return (
+    <>
+      <h1>Hello Vite React App</h1>;
+      <Appel />;
+    </>
+  );
 }
